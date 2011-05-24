84a204e867f9acbabdfd2da180554d052de6db85
benthor <benthor@os3.nl>
2011-05-24 21:41:14 +0200

    dictator now should support many of the classical dict() ways of object instantiation

--- dictator.py
+++ dictator.py
@@ -1,7 +1,17 @@
 class dictator(dict):
     """provides nice lua-like syntactic sugar for python dictionaries"""
-    def __init__(self, *args, **kwargs):
-        dict.__init__(self, kwargs)
+    def __init__(self, arg=[], **kwargs):
+        # jumping through a couple of hoops to allow fancy initialization in
+        # classical dictionary style
+        d = {}
+        if isinstance(arg, dict):
+            for k in arg:
+                d[k] = arg[k]
+        elif isinstance(arg, list):
+            print arg
+            for k,v in arg:
+                d[k] = v
+        dict.__init__(self, d, **kwargs)
 
     def __getattribute__(self, name):
         return dict.__getitem__(self, name)
