b3b9421e75aaac913456d300061ca0157a01cb72
benthor <benthor@os3.nl>
2011-05-24 21:57:38 +0200

    *facepalm*. if you want to stay faithful to your superclass's constructor, what do you do? OF COURSE: don't override it. dammit!

--- dictator.py
+++ dictator.py
@@ -1,19 +1,9 @@
 class dictator(dict):
     """provides nice lua-like syntactic sugar for python dictionaries"""
-    def __init__(self, arg=[], **kwargs):
-        # jumping through a couple of hoops to allow fancy initialization in
-        # classical dictionary style
-        d = {}
-        if isinstance(arg, dict):
-            for k in arg:
-                d[k] = arg[k]
-        elif isinstance(arg, list):
-            for k,v in arg:
-                d[k] = v
-        dict.__init__(self, d, **kwargs)
-
     def __getattribute__(self, name):
         return dict.__getitem__(self, name)
 
     def __setattr__(self, name, value):
         return dict.__setitem__(self, name, value)
+
+
